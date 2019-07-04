cf16a84462d10d58163a4bf3938c0ea22e98da01
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-07-04 09:37:50 +0200

    Add files via upload

--- ft_linked_list.c
+++ ft_linked_list.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 14:10:06 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/06/25 17:09:15 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/06/26 12:36:47 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 //Printing out all items from the linked list.
 
-void	printlist(node_t *head)
+void	ft_print_list(node_t *head)
 {
 	node_t *current = head;
 
@@ -83,17 +83,81 @@ void	ft_add_last_item(node_t *head)
 	current->next = NULL;
 }
 
+//Removing the last item from the linked list
+
+int		ft_remove_last_item(node_t *head)
+{
+	int		retval;
+	node_t	*current;
+
+	retval = 0;
+	current = NULL;
+	if (head->next == NULL)
+	{
+		retval = head->val;
+		free(head);
+		return (retval);
+	}
+
+	current = head;
+	while (current->next->next != NULL)
+	{
+		current = current->next;
+	}
+	retval = current->next->val;
+	free(current->next);
+	current->next = NULL;
+	return (retval);
+}
+
+int		ft_remove_first_item(node_t **head)
+{
+	node_t	*next_item;
+	int		retval;
+
+	retval = -1;
+	next_item = NULL;
+	if (*head == NULL)
+		return (retval);
+	next_item = (*head)->next;
+	retval = (*head)->val;
+	*head = next_item;
+	return (retval);
+}
+
+int		ft_remove_item_at(node_t *head, int num)
+{
+	int		retval;
+	int		count;
+	node_t	*temp;
+
+	retval = 0;
+	count = -1;
+
+	if (n == 0)
+		return (ft_remove_first_item(&head));
+	while (++count < num - 1)
+	{
+		if (temp->next)
+	}
+}
+
 int		main(void)
 {
 	node_t *head = NULL;
 
 	head = malloc(sizeof(node_t));
 	if (head == NULL)
-		return (1);
+		return (0);
 	head->val = 1;
 	head->next = NULL;
 	ft_add_last_item(head);
-	printlist(head);
+	ft_add_first_item(&head, 0);
+	ft_print_list(head);
+	printf("First Removed Item : %d\n", ft_remove_first_item(&head));
+	ft_print_list(head);
+	printf("Last Removed Item : %d\n", ft_remove_last_item(head));
+	ft_print_list(head);
 	free(head);
 	return (0);
 }
